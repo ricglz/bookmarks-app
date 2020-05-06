300a20ea0bcd89eb6055e2c69cec7f5089caf8d6
Ricardo J. González C <rjgcastillo@hotmail.com>
2020-05-06 11:38:44 -0500

    Add missing endpoints

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,9 @@
   const TOKEN = '2abbf7c3-245b-404f-9473-ade729ed4653'
   const results = document.querySelector('section.results')
 
-  function createBookmark({ id, title, description, url, rating }) {
+  function createBookmarkElement({ id, title, description, url, rating }) {
     const bookmark = document.createElement('div');
+    bookmark.className = 'result-item'
     bookmark.innerHTML = `
       <h4>${id} - ${title}</h4>
       <p>
@@ -15,7 +16,25 @@
     return bookmark;
   }
 
-  async function fetchAllStudents() {
+  async function fetchJSON(url, config) {
+    const response = await fetch(url, config);
+    if(!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return await response.json();
+  }
+
+  async function fetchGetEndpoint(url, config) {
+    try {
+      const json = await fetchJSON(url, config);
+      results.innerHTML = ''
+      json.forEach((bookmark) => { results.append(createBookmarkElement(bookmark)) })
+    } catch (err) {
+      results.innerHTML = `${err.message}`
+    }
+  }
+
+  function fetchAllStudents() {
     const url = '/bookmarks';
     const config = {
       method: 'GET',
@@ -23,35 +42,109 @@
         Authorization: `Bearer ${TOKEN}`
       }
     }
+    fetchGetEndpoint(url, config);
+  }
+
+  async function fetchAndModify(url, config) {
     try {
-      const response = await fetch(url, config);
-      if(!response.ok) {
-        throw new Error(response.statusText);
-      }
-      const json = await response.json();
-      results.innerHTML = ''
-      json.forEach((bookmark) => { results.append(createBookmark(bookmark)) })
+      await fetchJSON(url, config);
+      fetchAllStudents();
     } catch (err) {
       results.innerHTML = `${err.message}`
     }
   }
 
+  function createBookmark(data) {
+    const url = '/bookmarks';
+    const config = {
+      method : 'POST',
+      headers : {
+        Authorization : `Bearer ${TOKEN}`,
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify( data )
+    }
+    fetchAndModify(url, config);
+  }
+
+  async function deleteBookmark({ id }) {
+    const url = `/bookmark/${id}`;
+    const config = {
+      method : 'DELETE',
+      headers : {
+        Authorization : `Bearer ${TOKEN}`,
+      },
+    }
+    try {
+      const response = await fetch(url, config);
+      if(!response.ok) {
+        throw new Error(response.statusText);
+      }
+      fetchAllStudents();
+    } catch (err) {
+      results.innerHTML = `${err.message}`
+    }
+  }
+
+  function updateBookmark(data) {
+    const { id } = data;
+    const url = `/bookmark/${id}`;
+    const config = {
+      method : 'PATCH',
+      headers : {
+        Authorization : `Bearer ${TOKEN}`,
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(data),
+    }
+    fetchAndModify(url, config);
+  }
+
+  function getBookmarksByTitle(data) {
+    const params = new URLSearchParams(data);
+    const url = `/bookmark?${params}`;
+    const config = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }
+    fetchGetEndpoint(url, config);
+  }
+
+  function getFormInputValues(form) {
+    const values = {};
+    ['id', 'title', 'description', 'url', 'rating'].forEach((field) => {
+      const fieldInput = form.querySelector(`input#${field}`);
+      if(!fieldInput || fieldInput.value === '') {
+        return;
+      }
+      values[field] = fieldInput.value
+    });
+    return values;
+  }
+
+  function fetchEndpointWithFields(form, callToFetch) {
+    const values = getFormInputValues(form);
+    callToFetch(values);
+  }
+
   function addListeners() {
-    document.querySelector('form.create-bookmark')
-            .addEventListener('submit', function(e) {
+    document.querySelector('form.create-bookmark').addEventListener('submit', function(e) {
       e.preventDefault();
+      fetchEndpointWithFields(this, createBookmark);
     });
-    document.querySelector('form.delete-bookmark')
-            .addEventListener('submit', function(e) {
+    document.querySelector('form.delete-bookmark').addEventListener('submit', function(e) {
       e.preventDefault();
+      fetchEndpointWithFields(this, deleteBookmark);
     });
-    document.querySelector('form.update-bookmark')
-            .addEventListener('submit', function(e) {
+    document.querySelector('form.update-bookmark').addEventListener('submit', function(e) {
       e.preventDefault();
+      fetchEndpointWithFields(this, updateBookmark);
     });
-    document.querySelector('form.get-bookmark')
-            .addEventListener('submit', function(e) {
+    document.querySelector('form.get-bookmark').addEventListener('submit', function(e) {
       e.preventDefault();
+      fetchEndpointWithFields(this, getBookmarksByTitle);
     });
   }
 
